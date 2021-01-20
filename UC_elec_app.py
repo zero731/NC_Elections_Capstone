724431711aeba07a743f25d76a33788c9b02a777
zero731 <maxsteele731@gmail.com>
2021-01-20 14:06:53 -0500

    fix requirements.txt for app deployment

--- UC_elec_app.py
+++ UC_elec_app.py
@@ -1,7 +1,7 @@
 ## Import necessary libraries
 import streamlit as st
 import pandas as pd
-import numpy as np
+# import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
